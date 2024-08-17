583fd1ae78324f1512c936742f1dfa70c7825436
Alex Wine <alex.wine@truebuiltsoftware.com>
2024-08-16 18:22:00 -0600

    fix: updated tasks

--- apps/sample-nextjs/src/components/Sample/index.tsx
+++ apps/sample-nextjs/src/components/Sample/index.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { useRef } from 'react';
 import { GameOfLife } from '../GameOfLife';
 
 export const SampleThing = () => {
-	const display = useRef<HTMLPreElement>(null);
 	const loadWasm = async () => {
 		const wasmModule = await import('../../../../../crates/sample-wasm/pkg/sample_wasm');
 		return wasmModule;
