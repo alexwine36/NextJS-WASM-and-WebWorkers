8480116d62dcd863abf982f67ac770fda64f511d
Alex Wine <alex.wine@truebuiltsoftware.com>
2024-08-19 10:52:37 -0600

    fix: support for wasm server side
    https://github.com/vercel/next.js/issues/25852#issue-913503496

--- apps/sample-nextjs/next.config.mjs
+++ apps/sample-nextjs/next.config.mjs
@@ -1,12 +1,48 @@
+import { access, symlink } from 'fs/promises';
+import { join } from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack(config, { isServer, dev }) {
+	webpack(config, { isServer }) {
 		// Use the client static directory in the server bundle and prod mode
 		// Fixes `Error occurred prerendering page "/"`
 		// config.output.webassemblyModuleFilename =
 		// 	isServer && !dev ? '../static/pkg/[modulehash].wasm' : 'static/pkg/[modulehash].wasm';
-		// Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
 		config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+		config.plugins.push(
+			new (class {
+				apply(compiler) {
+					compiler.hooks.afterEmit.tapPromise('SymlinkWebpackPlugin', async (compiler) => {
+						if (isServer) {
+							const from = join(compiler.options.output.path, '../static');
+							const to = join(compiler.options.output.path, 'static');
+
+							try {
+								await access(from);
+								console.log(`${from} already exists`);
+								return;
+							} catch (error) {
+								if (error.code === 'ENOENT') {
+									// No link exists
+								} else {
+									throw error;
+								}
+							}
+
+							await symlink(to, from, 'junction');
+							console.log(`created symlink ${from} -> ${to}`);
+						}
+					});
+				}
+			})(),
+		);
+		// https://github.com/vercel/next.js/issues/25852
+		if (isServer) {
+			config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+		} else {
+			config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+		}
 		return config;
 	},
 };
