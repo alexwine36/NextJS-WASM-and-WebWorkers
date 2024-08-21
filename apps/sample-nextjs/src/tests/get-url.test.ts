8567c3ff177a0e491e0098b34bb8f77d9fa4e743
Alex Wine <alex.wine@truebuiltsoftware.com>
2024-08-21 16:46:20 -0600

    fix: updated url test

--- apps/sample-nextjs/src/tests/get-url.test.ts
+++ apps/sample-nextjs/src/tests/get-url.test.ts
@@ -4,10 +4,10 @@ describe('first', () => {
 	test('should first', () => {
 		const url = new LocalUrl('/api/route');
 
-		expect(url.toString()).toBe('http://localhost:3001/api/route');
+		expect(url.toString()).toBe('http://localhost:3000/api/route');
 
 		url.addQuery('query', '12');
 
-		expect(url.toString()).toBe('http://localhost:3001/api/route?query=12');
+		expect(url.toString()).toBe('http://localhost:3000/api/route?query=12');
 	});
 });
